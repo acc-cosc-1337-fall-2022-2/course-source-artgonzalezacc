882b1918ddb2d850aa777578c95a69a722ee85a6
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-09-14 01:03:32 +0000

    References, value and reference func params

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,20 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    int num = 10;
+    int& num_ref = num;//this means get the memory address where num is stored
+    cout<<"Display the value stored in num via num_ref: "<<num_ref<<"\n";
+    
+    cout<<"Change num value via num_ref: \n";
+    
+    num_ref = 20;
+    cout<<"Display the value stored in num via num_ref: "<<num_ref<<"\n";
+    cout<<"Display the value of num: "<<num<<"\n";
+    cout<<"Display the address of num: "<<&num<<"\n";
+    cout<<"Display the address of num_ref: "<<&num_ref<<"\n";
+
     return 0;
 }
---
+++ src/examples/05_functions/03_val_ref_params/val_ref.cpp
@@ -0,0 +1,5 @@
+void val_ref_params(int num, int& num1)
+{
+    num = 20;
+    num1 = 30;
+}
