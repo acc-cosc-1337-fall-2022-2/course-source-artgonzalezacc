2f897d7dba57d96fa681ddbeb1cd6780b731a8ce
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-09-28 04:07:00 +0000

    class constructors, public and private var/func

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,1 +1,14 @@
 //bank_account.h
+class BankAccount
+{
+public://other code(like main, Test cases, classes) can call public functions
+    //they can be called from THIS class private or pubic functions
+    //a class constructor
+    BankAccount(){}//default constructor
+    BankAccount(int b) : balance(b){/*empty function block*/}
+    int get_balance() const { return balance;}
+
+private://access specifier-only  THIS class private or public functions have access to
+         //private variables or functions
+    int balance;// BankAccount wants to protect its data(variables)
+};
--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,6 +1,25 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "bank_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test bank account get balance with no constructor param")
+{
+	//assuming bankacountdb existed and we retrieved balance from there
+
+	BankAccount account;//create an instance/variable of the BankAccount class
+
+	REQUIRE(account.get_balance() == 0);
+}
+
+TEST_CASE("Test bank account get balance with constructor param")
+{
+	//assuming bankacountdb existed and we retrieved balance from there
+
+	BankAccount account(100);//create an instance/variable of the BankAccount class
+
+	REQUIRE(account.get_balance() == 100);
 }
