2d37f82ad566c187a301f41dcacd667db9e973b6
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-10-17 22:41:33 +0000

    abstract shape

--- src/examples/08_classes/02_shapes/main.cpp
+++ src/examples/08_classes/02_shapes/main.cpp
@@ -11,8 +11,23 @@ Create vector of Shape pointers
 iterate with auto
 */
 
+using std::vector; using std::cout;
+
 int main() 
 {
+	Shape* shape = new Circle();
+	Shape* shape1 = new Line();	
+
+	vector<Shape*> shapes{shape, shape1};
+
+	for(auto shape: shapes)
+	{
+		shape->draw();
+	}
+
+	delete shapes[0];
+	delete shapes[1];
+
 
 	return 0;
 }
