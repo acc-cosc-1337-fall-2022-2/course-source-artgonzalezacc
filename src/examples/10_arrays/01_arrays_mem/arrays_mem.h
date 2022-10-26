87abc83709a1a75c906499032bcc90797b549498
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-10-26 23:52:33 +0000

    stack int, string, arrays n pointers

--- src/examples/10_arrays/01_arrays_mem/arrays_mem.h
+++ src/examples/10_arrays/01_arrays_mem/arrays_mem.h
@@ -1,3 +1,9 @@
 //arrays_mem.h - stack array example
+#include<iostream>
+#include<string>
 
 //function prototype for stack_array
+void use_stack_array();
+void array_months();
+void arrays_and_pointers();
+void display_array(int* nums, const int SIZE);
--- src/examples/10_arrays/01_arrays_mem/main.cpp
+++ src/examples/10_arrays/01_arrays_mem/main.cpp
@@ -1,9 +1,14 @@
-
+#include "arrays_mem.h"
 
 int main() 
 {
+	//use_stack_array();
+	//array_months();
+	//arrays_and_pointers();
 
-	
+	const auto SIZE = 3;//allocate 1 4 byte memory 
+    int numbers[SIZE]{1,2,3};//allocate 3 continuous memory addresses
+	display_array(numbers, SIZE);
 
 	return 0;
 
