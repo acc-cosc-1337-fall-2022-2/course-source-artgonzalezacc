0ed2f3d7172f38a01130cf0c28b69fef03f12a49
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-10-31 22:40:54 +0000

    object stack arrays and 2d

--- src/examples/10_arrays/05_arrays_functions/arr_functions.cpp
+++ src/examples/10_arrays/05_arrays_functions/arr_functions.cpp
@@ -1,4 +1,29 @@
 //
-#include<iostream>
 #include "arr_functions.h"
 //define iterate_array and loop through with ++ increment
+
+using std::cout; using std::setw;
+
+void populate_times_table(int times_table[][COLS], const int ROWS)
+{
+    for(auto i = 0 ; i < ROWS; i++)
+    {
+        for(auto j=0; j < COLS; j++)
+        {
+            times_table[i][j] = (i+1) * (j+1);
+        }
+    }
+}
+
+void display_times_table(int times_table[][COLS], const int ROWS)
+{
+    for(auto i=0; i < ROWS; i++)
+    {
+        for(auto j=0; j < COLS; j++)
+        {
+            cout<<setw(5)<<times_table[i][j];
+        }
+
+        cout<<"\n";
+    }
+}
--- src/examples/10_arrays/05_arrays_functions/main.cpp
+++ src/examples/10_arrays/05_arrays_functions/main.cpp
@@ -1,8 +1,12 @@
 #include "arr_functions.h"
 
+
 int main() 
 {
-	
+	int times_table[ROWS][COLS];
+
+	populate_times_table(times_table, ROWS);
+	display_times_table(times_table, ROWS);
 
 	return 0;
 }
